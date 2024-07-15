539ca407e9467fb76ab9d4dfd0babdac7bd5dc89
peugeot <thomaspeugeot@yahoo.fr>
2024-07-15 15:16:03 +0200

    40''

--- go/probe/fill_up_table.go
+++ go/probe/fill_up_table.go
@@ -20,12 +20,8 @@ func fillUpTable[T models.Gongstruct](
 	probe *Probe,
 ) {
 
-	table := new(gongtable.Table).Stage(probe.tableStage)
-
 	fields := models.GetFields[T]()
 	reverseFields := models.GetReverseFields[T]()
-
-	table.NbOfStickyColumns = 3
 
 	// refresh the stage of interest
 	probe.stageOfInterest.Checkout()
@@ -35,8 +31,6 @@ func fillUpTable[T models.Gongstruct](
 	for _, structInstance := range sliceOfGongStructsSorted {
 		row := new(gongtable.Row).Stage(probe.tableStage)
 		row.Name = models.GetFieldStringValue[T](*structInstance, "Name")
-
-		table.Rows = append(table.Rows, row)
 
 		for _, fieldName := range fields {
 			value := models.GetFieldStringValue[T](*structInstance, fieldName)
