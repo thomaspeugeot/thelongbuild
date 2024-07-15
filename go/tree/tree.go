309081747f0146a88c2f8465039d4df064d4ce71
peugeot <thomaspeugeot@yahoo.fr>
2024-07-15 09:11:57 +0200

    43''

--- go/tree/tree.go
+++ go/tree/tree.go
@@ -6,16 +6,10 @@ import (
 	table "github.com/fullstack-lang/gongtable/go/models"
 	tree "github.com/fullstack-lang/gongtree/go/models"
 
-	gongtree_stack "github.com/fullstack-lang/gongtree/go/stack"
-
 	"github.com/thomaspeugeot/thelongbuild/go/models"
 
 	thelongbuild_probe "github.com/thomaspeugeot/thelongbuild/go/probe"
 )
-
-type ModelObject interface {
-	GetName() string
-}
 
 func NewNodeImpl[T2 models.GenericNode[T], T models.Gongstruct](
 	treeWs *TreeWs,
@@ -44,18 +38,11 @@ func (nodeCallback *NodeImpl[T2, T]) OnAfterUpdate(stage *tree.StageStruct, old,
 
 // TreeWs, for tree workspace holds the supporting data for performing operation on the weber tree
 type TreeWs struct {
-	TreeStack *gongtree_stack.Stack
-
-	NodeTree *tree.Tree
-
-	// AllDiagramNodes is a pratical way to access all diagrams nodes
-	// tree
-	AllDiagramNodes []*tree.Node
 }
 
 func (treeWs *TreeWs) GenerateTree() {
 
 	bridge := new(models.TheBridge)
-	analysisNode := new(tree.Node).Stage(treeWs.TreeStack.Stage)
+	analysisNode := new(tree.Node)
 	analysisNode.Impl = NewNodeImpl(treeWs, bridge)
 }
